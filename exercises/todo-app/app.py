1f2ee9771119bb607dc72715bf050bebb10ae625
ahgarawani <ahgarawani@gmail.com>
2020-08-13 15:48:11 +0200

    adding the completed functionality

--- exercises/todo-app/app.py
+++ exercises/todo-app/app.py
@@ -40,9 +40,29 @@ def create_todo():
         return jsonify(body)
 
 
+@app.route('/todos/<todo_id>/set-completed', methods=['POST'])
+def set_completed_todo(todo_id):
+    error = False
+    try:
+        completed = request.get_json()['completed']
+        todo = Todo.query.get(todo_id)
+        todo.completed = completed
+        db.session.commit()
+    except:
+        error = True
+        db.session.rollback()
+        print(sys.exc_info())
+    finally:
+        db.session.close()
+    if error:
+        abort (500)
+    else:
+        return redirect(url_for('index'))
+
+
 @app.route('/')
 def index():
-    return render_template('index.html', data=Todo.query.all())
+    return render_template('index.html', data=Todo.query.order_by('id').all())
 
 if __name__ == '__main__':
     app.run()
